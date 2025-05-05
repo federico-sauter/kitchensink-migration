d1601c859925de3f7b360f6ba76bdb17ea37630e
Federico Sauter <github.shrug288@passinbox.com>
2025-05-05 16:03:32 +0000

    Migrate POST members

--- app/src/main/java/com/mongodb/kitchensink/repository/MemberRepository.java
+++ app/src/main/java/com/mongodb/kitchensink/repository/MemberRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
   List<Member> findAllByOrderByNameAsc();
+
+  boolean existsByEmail(String email);
 }
