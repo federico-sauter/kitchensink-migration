23e87f95216ce80ca52d8ef232faf0c674f35a28
Federico Sauter <github.shrug288@passinbox.com>
2025-05-05 10:52:18 +0000

    Migrated the /rest/members endpoint to Spring Boot

---
+++ app/src/main/java/com/mongodb/kitchensink/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.mongodb.kitchensink.repository;
+
+import com.mongodb.kitchensink.model.Member;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+  List<Member> findAllByOrderByNameAsc();
+}
