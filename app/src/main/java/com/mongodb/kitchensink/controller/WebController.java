af8dbd044dbc65953d5ed77bdc6533619a36f452
Federico Sauter <github.shrug288@passinbox.com>
2025-05-06 11:00:33 +0000

    Demo Web UI implementation

---
+++ app/src/main/java/com/mongodb/kitchensink/controller/WebController.java
@@ -0,0 +1,12 @@
+package com.mongodb.kitchensink.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+  @GetMapping("/ui/members")
+  public String membersPage() {
+    return "members";     // resolves to templates/members.html
+  }
+}
